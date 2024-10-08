5d41067484926f21d20b76281cd5139574cc5d7a
Mileek <kamilex1998@gmail.com>
2024-10-08 20:19:48 +0200

    Add GeolocationDbContext and GeolocationModel classes and structure. InMemory Database for testing purposes.

---
+++ Data/GeolocationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DesktopAssignment.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DesktopAssignment.Data
+{
+    public class GeolocationDbContext : DbContext
+    {
+        public DbSet<GeolocationModel> Geolocations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //InMemory database for testing purposes
+            optionsBuilder.UseInMemoryDatabase("GeolocationDb");
+        }
+    }
+}
